fa0cd9d52e1715a1956ddcddcfead86d84f3cff0
Tim Harvey <timharveydev@gmail.com>
2020-12-31 13:13:15 +0000

    Create admin change users page and styling. Create data-table component.

---
+++ admin-change-users.php
@@ -0,0 +1,189 @@
+<?php
+
+session_start();
+
+// Stores current URL minus arguments
+$_SESSION['redirect'] = strtok($_SERVER['REQUEST_URI'], '?');
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- METAS & TITLE
+  --------------------------------------------------------->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="author" content="Tim Harvey">
+  <meta name="description" content="A dynamic cinema website created for my HND Web Development course.">
+  <title>Limelight | Admin Panel</title>
+
+  <!-- CSS
+  --------------------------------------------------------->
+  <link rel="stylesheet" href="http://webdev.edinburghcollege.ac.uk/~HNCWEBMR4/limelight-cinemas/css/main.css">
+
+  <!-- FONTS
+  --------------------------------------------------------->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
+
+  <!-- FONT AWSOME
+  --------------------------------------------------------->
+  <script src="https://kit.fontawesome.com/6cc30ac4de.js" crossorigin="anonymous"></script>
+
+  <!-- FAVICON
+  --------------------------------------------------------->
+  <link rel="icon" type="image/png" sizes="32x32" href="http://webdev.edinburghcollege.ac.uk/~HNCWEBMR4/limelight-cinemas/img/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="http://webdev.edinburghcollege.ac.uk/~HNCWEBMR4/limelight-cinemas/img/favicon/favicon-16x16.png">
+  <link rel="manifest" href="http://webdev.edinburghcollege.ac.uk/~HNCWEBMR4/limelight-cinemas/img/favicon/site.webmanifest">
+  <link rel="mask-icon" href="http://webdev.edinburghcollege.ac.uk/~HNCWEBMR4/limelight-cinemas/img/favicon/safari-pinned-tab.svg" color="#49b171">
+
+</head>
+<body id="top">
+
+  <!-- MAIN CONTENT
+  --------------------------------------------------------->
+
+  <!-- Navigation (Mobile-specific stuff removed - admin section not used on mobile devices)
+  ------------------------------------->
+  <nav class="nav">
+    <div class="nav__container container">
+
+      <div class="nav__logo">
+        <a href="index.php"><img src="http://webdev.edinburghcollege.ac.uk/~HNCWEBMR4/limelight-cinemas/img/logo.svg" alt="Limelight Cinemas Logo"></a>
+      </div>
+
+      <ul class="nav__list">
+        <li class="nav__item"><a href="index.php#films" class="nav__link">What's On?</a></li>
+        <li class="nav__item"><a href="activities.php" class="nav__link">Activities</a></li>
+        <li class="nav__item"><a href="about.php" class="nav__link">About</a></li>
+        <li class="nav__item"><a href="contact.php" class="nav__link">Contact</a></li>
+
+        <!-- Admin panel button - for large devices only -->
+        <li class="nav__item mobile-hidden">
+          <a class="nav__button button" href="admin-home.php">Admin Panel</a>
+        </li>
+
+        <!-- Logout button (login button not needed - admin pages not accessible unless logged in) -->
+        <li class="nav__item mobile-hidden">
+          <a class="nav__button button--negative" href="logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a>
+        </li>
+      </ul>
+
+    </div>
+  </nav>
+
+
+
+
+
+  <!-- Admin panel content
+  ------------------------------------->
+  <section class="admin-change-users">
+    <div class="admin-change-users__container container">
+
+      <h1 class="admin-change-users__heading">Change / Remove Users</h1>
+
+
+
+      <!-- Search bar component -->
+      <form class="admin-change-users__search-bar search-bar" action="#" method="#">
+        
+        <input type="text" name="search" class="search-bar__input" placeholder="Search user details ...">
+
+        <!-- Search button for most devices -->
+        <button type="submit" name="submit" class="search-bar__button button--positive"><i class="fas fa-search"></i> Search</button>
+
+      </form>
+
+
+
+      <!-- Success confirmation - shown when user details changed -->
+      <?php
+
+      if ($_GET['success'] == 'success') {
+        echo '<span class="admin-change-users__success">Changes successful</span>';
+      }
+
+      ?>
+
+
+
+      <!-- Users table (data table component) -->
+      <div class="admin-change-users__data-table data-table">
+
+      <!-- Table headings -->
+      <form class="data-table__form" action="" method="POST">
+          <input type="text" class="data-table__heading" value="Username" readonly>
+          <input type="text" class="data-table__heading" value="Password" readonly>
+          <input type="text" class="data-table__heading" value="Date of Birth" readonly>
+          <input type="text" class="data-table__heading" value="Email" readonly>
+          <input type="submit" class="data-table__button--hidden button--primary" value="Update" readonly>
+          <input type="submit" class="data-table__button--hidden button--negative" value="Delete" readonly>
+        </form>
+        <hr>
+
+        <!-- Table content - PHP creates separate form for each user, taking info from DB -->
+        <form class="data-table__form" action="" method="POST">
+          <input type="text" class="data-table__input" value="Username">
+          <input type="text" class="data-table__input" value="Password">
+          <input type="text" class="data-table__input" value="DoB">
+          <input type="text" class="data-table__input" value="Email">
+          <input type="hidden" class="data-table__input" value="ID">
+          <input type="submit" class="data-table__button button--primary" value="Update">
+          <input type="submit" class="data-table__button button--negative" value="Delete">
+        </form>
+
+      </div>
+
+
+    </div>
+  </section>
+
+
+
+
+
+  <!-- Footer
+  ------------------------------------->
+  <footer class="footer">
+    <div class="footer__container container">
+
+      <div class="footer__nav">
+        <a href="index.php#films" class="footer__link">What's On?</a>
+        <a href="activities.php" class="footer__link">Activities</a>
+        <a href="about.php" class="footer__link">About</a>
+        <a href="contact.php" class="footer__link">Contact</a>
+      </div>
+
+
+      <div class="footer__flex-wrapper">
+
+        <div class="footer__social">
+          <a class="footer__social--icon" href="#"><i class="fab fa-facebook-f"></i></a>
+          <a class="footer__social--icon" href="#"><i class="fab fa-youtube"></i></a>
+          <a class="footer__social--icon" href="#"><i class="fab fa-twitter"></i></a>
+        </div>
+
+        <div class="footer__copyright">
+          &copy; 2020 Limelight Cinemas. All Rights Reserved.
+        </div>
+
+      </div>
+
+
+      <div class="footer__link-to-top">
+        <a href="#top" class="footer__link">Back to top</a>
+      </div>
+
+    </div>
+  </footer>
+
+
+  <!-- END DOCUMENT
+  --------------------------------------------------------->
+  
+</body>
+</html>
